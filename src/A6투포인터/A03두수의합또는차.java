62b23f95d648e8113c53b6ef9e573b9d6c0a36ea
JAYDEN <aphorism037@naver.com>
2025-07-03 22:51:51 +0900

    백준 과자, 수고르기

--- src/A6투포인터/A03두수의합또는차.java
+++ src/A6투포인터/A03두수의합또는차.java
@@ -1,36 +1,66 @@
 package A6투포인터;
 
 import java.util.Arrays;
-import java.util.Map;
+import java.util.Scanner;
 
 public class A03두수의합또는차 {
     public static void main(String[] args) {
 //        두용액(2470) : 백준
-        int N = 5;  //전체 용액의 수
-        int[] nums = {-2, 4, -99, -1, 98};
-        Arrays.sort(nums);
-
-        int start = 0;
-        int end = nums.length-1;
-        int result = Integer.MAX_VALUE;
-
-        int ans1 = 0;
-        int ans2 = 0;
-        while (start < end) {
-            int total = nums[start] + nums[end];
-
-            if (Math.abs(total) < result) {
-                result = Math.abs(total);
-                ans1 = nums[start];
-                ans2 = nums[end];
-            }
-            if (total < 0) {
-                start++;
-            }else
-                end--;
-        }
-        System.out.println(ans1 + " " + ans2);
+//        int N = 5;  //전체 용액의 수
+//        int[] nums = {-2, 4, -99, -1, 98};
+//        Arrays.sort(nums);
+//
+//        int start = 0;
+//        int end = nums.length-1;
+//        int result = Integer.MAX_VALUE;
+//
+//        int ans1 = 0;
+//        int ans2 = 0;
+//        while (start < end) {
+//            int total = nums[start] + nums[end];
+//
+//            if (Math.abs(total) < result) {
+//                result = Math.abs(total);
+//                ans1 = nums[start];
+//                ans2 = nums[end];
+//            }
+//            if (total < 0) {
+//                start++;
+//            }else
+//                end--;
+//        }
+//        System.out.println(ans1 + " " + ans2);
 
 //        수고르기 : 백준
+        Scanner sc = new Scanner(System.in);
+        String[] line1 = sc.nextLine().split(" ");
+        int N = Integer.parseInt(line1[0]);
+        int M = Integer.parseInt(line1[1]);
+
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+        Arrays.sort(A);
+
+        int start = 0;
+        int end = 0;
+        int minTarget = Integer.MAX_VALUE;
+
+        while (end < N) {
+            if (start == end) {
+                end++;
+                continue;
+            }
+            int diff = A[end] - A[start];
+
+            if (diff >= M) {
+                minTarget = Math.min(minTarget, diff);
+                start++;    // 차이 더 줄여보기
+            } else {
+                end++;
+            }
+        }
+        System.out.println(minTarget);
     }
 }
--- src/A7이분탐색/A02이분탐색활용문제.java
+++ src/A7이분탐색/A02이분탐색활용문제.java
@@ -1,37 +1,72 @@
 package A7이분탐색;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class A02이분탐색활용문제 {
     public static void main(String[] args) {
 //        예산 : 백준
-        int[] budgets = {120, 110, 140, 150};
-        int target = 485;
+//        int[] budgets = {120, 110, 140, 150};
+//        int target = 485;
+//
+//        Arrays.sort(budgets);
+//        int start = 0;
+//        int end = budgets[budgets.length-1];
+//        int answer = 0;
+//
+//        while (start <= end) {
+//            int mid = (start + end) / 2;
+//
+//            long sum = 0;
+//            for (int b : budgets) {
+//                //핵심 코드 : 상한선과 각 국가예산을 비교해서 예산 배정을 상한선을 넘지 않도록 하는 코드
+//                sum += Math.min(b, mid);
+//            }
+//
+//            if (sum <= target) {    // sum이 전체예산 target을 넘지 않으면 상한선 +, 아니면 상한선 -
+//                answer = mid;
+//                start = mid + 1;
+//            } else {
+//                end = mid - 1;
+//            }
+//        }
+//        System.out.println(answer);
 
-        Arrays.sort(budgets);
-        int start = 0;
-        int end = budgets[budgets.length-1];
-        int answer = 0;
+//        과자나눠주기 : 백준 - answer 찾은 후에도 계속 탐색
+        Scanner sc = new Scanner(System.in);
+        String[] line1 = sc.nextLine().split(" ");
+        int M = Integer.parseInt(line1[0]); //조카의 수
+        int N = Integer.parseInt(line1[1]); //과자의 수
+
+        String[] line2 = sc.nextLine().split(" ");
+        int[] snacks = new int[N];
+        for (int i = 0; i < N; i++) {
+            snacks[i] = Integer.parseInt(line2[i]);
+        }
+
+        Arrays.sort(snacks);
+
+        int start = 1;
+        int end = snacks[snacks.length - 1];
+        long maxLength = 0;
 
         while (start <= end) {
             int mid = (start + end) / 2;
 
-            long sum = 0;
-            for (int b : budgets) {
-                //핵심 코드 : 상한선과 각 국가예산을 비교해서 예산 배정을 상한선을 넘지 않도록 하는 코드
-                sum += Math.min(b, mid);
+            long total = 0;
+            for (int snack : snacks) {
+                total += snack / mid;
             }
 
-            if (sum <= target) {    // sum이 전체예산 target을 넘지 않으면 상한선 +, 아니면 상한선 -
-                answer = mid;
+            if (total >= M) {
+//                maxLength = total;
+                maxLength = mid;    //과자수가 아니라 나눈 최대길이를 저장
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        System.out.println(answer);
-
-//        과자나눠주기 : 백준 - answer 찾은 후에도 계속 탐색
+        System.out.println(maxLength);
 
 
 //        입국심사 : 백준
